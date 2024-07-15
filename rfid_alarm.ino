7e02f9dfb91a21d97ed3deafb230c252d3eaa7d0
Arturo Fonseca <49870466+arturo32@users.noreply.github.com>
2024-07-15 11:53:26 -0300

    Muda biblioteca do 522 para mfrc522v2

--- rfid_alarm.ino
+++ rfid_alarm.ino
@@ -8,10 +8,28 @@
  */
 
 #include <SPI.h>
-#include <MFRC522.h>
+// #include <MFRC522.h>
+#include <MFRC522v2.h>
+#include <MFRC522DriverSPI.h>
+//#include <MFRC522DriverI2C.h>
+#include <MFRC522DriverPinSimple.h>
+#include <MFRC522Debug.h>
 
-#define SS_PIN  5  // ESP32 pin GPIO5 
+#define SS_PIN 5  // ESP32 pin GPIO5 
 #define RST_PIN 21 // ESP32 pin GPIO27 
+
+MFRC522DriverPinSimple ss_pin(SS_PIN); // Configurable, see typical pin layout above.
+
+MFRC522DriverSPI driver{ss_pin}; // Create SPI driver.
+// MFRC522DriverI2C driver{}; // Create I2C driver.
+MFRC522 mfrc522{driver}; // Create MFRC522 instance.
+
+String tagContent = "";
+
+
+
+
+
 
 #include "pitches.h"
 #define BUZZER_PIN  22 // ESP32 pin GPIO18 connected to piezo buzzer
@@ -24,43 +42,57 @@ int melody[] = {
   NOTE_C8, NOTE_A7, NOTE_A7, NOTE_D8, NOTE_C7, 0, NOTE_C8, NOTE_C8
 };
 
-MFRC522 rfid(SS_PIN, RST_PIN);
-
 void setup() {
-  Serial.begin(9600);
-  SPI.begin(); // init SPI bus
-  rfid.PCD_Init(); // init MFRC522
+  Serial.begin(115200); // Initialize serial communications with the PC for debugging.
+  while (!Serial)
+    ;                                                     // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4).
+  mfrc522.PCD_Init();                                     // Init MFRC522 board.
+  MFRC522Debug::PCD_DumpVersionToSerial(mfrc522, Serial); // Show details of PCD - MFRC522 Card Reader details.
+  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 
-      for (int thisNote = 0; thisNote < 8; thisNote++) {
-      int noteDuration = 1000 / noteDurations[thisNote];
-      tone(BUZZER_PIN, melody[thisNote], noteDuration);
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(BUZZER_PIN, melody[thisNote], noteDuration);
 
-      int pauseBetweenNotes = noteDuration * 1.30;
-      delay(pauseBetweenNotes);
-      noTone(BUZZER_PIN);
-      }
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(BUZZER_PIN);
+  }
 
   Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader");
 }
 
-void loop() {
-  if (rfid.PICC_IsNewCardPresent()) { // new tag is available
-    if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
-      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-      Serial.print("RFID/NFC Tag Type: ");
-      Serial.println(rfid.PICC_GetTypeName(piccType));
 
-      // print UID in Serial Monitor in the hex format
-      Serial.print("UID:");
-      for (int i = 0; i < rfid.uid.size; i++) {
-        Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
-        Serial.print(rfid.uid.uidByte[i], HEX);
-      }
-      Serial.println();
-
-
-      rfid.PICC_HaltA(); // halt PICC
-      rfid.PCD_StopCrypto1(); // stop encryption on PCD
-    }
+void readRfid() {
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if (!mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
   }
+
+  // Select one of the cards.
+  if (!mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+
+  // Dump debug info about the card; PICC_HaltA() is automatically called.
+  MFRC522Debug::PICC_DumpToSerial(mfrc522, Serial, &(mfrc522.uid));
+
+  Serial.println("Printing only the Card ID:");
+  Serial.println("--------------------------");
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    tagContent.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    tagContent.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+
+  tagContent.toUpperCase();
+
+  Serial.println(tagContent);
+  tagContent = "";
+}
+
+void loop() {
+  readRfid();
 }
