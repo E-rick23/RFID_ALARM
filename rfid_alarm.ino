ea186db67d5b0d17365064d8397ab17ae2785599
Erick Marques <67922086+E-rick23@users.noreply.github.com>
2024-07-17 11:59:30 -0300

    Configurado alarme e permissões.

--- rfid_alarm.ino
+++ rfid_alarm.ino
@@ -1,35 +1,23 @@
 
-/*
- * This ESP32 code is created by esp32io.com
- *
- * This ESP32 code is released in the public domain
- *
- * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-rfid-nfc
- */
-
 #include <SPI.h>
-// #include <MFRC522.h>
 #include <MFRC522v2.h>
 #include <MFRC522DriverSPI.h>
-//#include <MFRC522DriverI2C.h>
 #include <MFRC522DriverPinSimple.h>
 #include <MFRC522Debug.h>
 
 #define SS_PIN 5  // ESP32 pin GPIO5 
 #define RST_PIN 21 // ESP32 pin GPIO27 
+#define RED_LED 12 
+#define GREEN_LED 14
+String VALID_ID = "73 2E 88 11";
 
 MFRC522DriverPinSimple ss_pin(SS_PIN); // Configurable, see typical pin layout above.
 
 MFRC522DriverSPI driver{ss_pin}; // Create SPI driver.
-// MFRC522DriverI2C driver{}; // Create I2C driver.
+
 MFRC522 mfrc522{driver}; // Create MFRC522 instance.
 
 String tagContent = "";
-
-
-
-
-
 
 #include "pitches.h"
 #define BUZZER_PIN  22 // ESP32 pin GPIO18 connected to piezo buzzer
@@ -44,25 +32,13 @@ int melody[] = {
 
 void setup() {
   Serial.begin(115200); // Initialize serial communications with the PC for debugging.
-  while (!Serial)
-    ;                                                     // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4).
   mfrc522.PCD_Init();                                     // Init MFRC522 board.
-  MFRC522Debug::PCD_DumpVersionToSerial(mfrc522, Serial); // Show details of PCD - MFRC522 Card Reader details.
-  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
-
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
-    int noteDuration = 1000 / noteDurations[thisNote];
-    tone(BUZZER_PIN, melody[thisNote], noteDuration);
-
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
-    noTone(BUZZER_PIN);
-  }
-
+  pinMode(12, OUTPUT);
+  pinMode(14, OUTPUT);
   Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader");
 }
 
-
+//Função que lê o cartão.
 void readRfid() {
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
   if (!mfrc522.PICC_IsNewCardPresent())
@@ -76,23 +52,47 @@ void readRfid() {
     return;
   }
 
-  // Dump debug info about the card; PICC_HaltA() is automatically called.
-  MFRC522Debug::PICC_DumpToSerial(mfrc522, Serial, &(mfrc522.uid));
-
-  Serial.println("Printing only the Card ID:");
+  Serial.println("Imprimindo o ID:");
   Serial.println("--------------------------");
   for (byte i = 0; i < mfrc522.uid.size; i++)
   {
     tagContent.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
     tagContent.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
-
+  tagContent.remove(tagContent.indexOf(' '), 1); //Remove o primeiro espaço do ID
   tagContent.toUpperCase();
-
+  permissionStatus(tagContent);
   Serial.println(tagContent);
   tagContent = "";
 }
 
+//Função que libera a tranca
+void permissionStatus(String tagContent){
+  if(tagContent == VALID_ID){
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(RED_LED, LOW);
+    Serial.println("true");
+    delay(1000);
+    digitalWrite(GREEN_LED, LOW);
+  }
+  else{
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(GREEN_LED, LOW);
+    Serial.println("false");
+    bool alarm = true;
+    while (alarm){
+      for (int thisNote = 0; thisNote < 8; thisNote++) {
+      int noteDuration = 1000 / noteDurations[thisNote];
+      tone(BUZZER_PIN, melody[thisNote], noteDuration);
+
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      noTone(BUZZER_PIN);
+      }
+      alarm = false;
+    }
+  }
+}
 void loop() {
   readRfid();
 }
